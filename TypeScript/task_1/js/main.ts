084b4391dc4c273e0aaf57f68b93cfc27ed7abea
SofianeSlimane <sofianeslimane24@gmail.com>
2024-09-23 19:57:22 +0200

    update: add Directors and printTeacherFunction interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -16,3 +16,33 @@ const teacher3: Teacher = {
   };
   
   console.log(teacher3);
+
+
+interface Directors extends Teacher {
+    numberOfReports: number,
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
+
+
+
+
+  interface printTeacherFunction{
+    printTeacher(firstName: string, lastName: string): string;
+  }
+
+
+  function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+
+  }
+
+  console.log(printTeacher("Dark", "Soso"));
